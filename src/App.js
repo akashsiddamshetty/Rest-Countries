2f4b4c9d711ae8d8369ceca57794b7a8f0bdd6c1
akashsiddamshetty <karthik245745@gmail.com>
2021-07-29 17:58:17 +0530

    new

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
         <Switch>
           <Route path="/" exact>
 
-            <Section />
+            <Section/>
           </Route>
           <Route path="/CountryDetails/:id" component={CountryDetails} />
         </Switch>
